9eecbececda969bd8f8c3f3b6114d49f8461fa91
MCris29 <crismax0629@gmail.com>
2020-07-21 09:33:13 -0500

    Movimiento de dados

--- js/lanzardados.js
+++ js/lanzardados.js
@@ -6,6 +6,18 @@ function lanzardados() {
 
     console.log(dado1 + "-" + dado2);
 
+    $({ deg: 0 }).animate({ deg: 360 }, {
+        duration: 600,
+        step: function (now) {
+            var scale = (1 * now / 360);
+            $('#ImgDado1').css({
+                transform: 'rotate(' + now + 'deg) scale(' + scale + ')'
+            });
+            $('#ImgDado2').css({
+                transform: 'rotate(' + now + 'deg) scale(' + scale + ')'
+            });
+        }
+    });
 
     document.getElementById("ImgDado1").src = "img/dados/" + dado1 + ".png";
     document.getElementById("ImgDado2").src = "img/dados/" + dado2 + ".png";
